f7da32762d0b1aae6538da26df34cdef73e32529
Khanh Du <luongkhanhdu95@gmail.com>
2024-09-30 13:29:30 +0700

    fix run with maven

--- product/src/main/java/com/fjb/product/repository/ProductRepository.java
+++ product/src/main/java/com/fjb/product/repository/ProductRepository.java
@@ -1,8 +1,8 @@
 package com.fjb.product.repository;
 
 import com.fjb.product.entity.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends MongoRepository<Product, String> {
+public interface ProductRepository extends JpaRepository<Product, String> {
 
 }
