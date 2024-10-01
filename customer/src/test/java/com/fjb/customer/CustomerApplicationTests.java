d739322c80c779ff5d6617757ab244ec980081f4
Khanh Du <luongkhanhdu95@gmail.com>
2024-10-01 13:23:35 +0700

    fix sonar

--- customer/src/test/java/com/fjb/customer/CustomerApplicationTests.java
+++ customer/src/test/java/com/fjb/customer/CustomerApplicationTests.java
@@ -1,13 +1,16 @@
 package com.fjb.customer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class CustomerApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("Just test");
     }
 
 }
--- inventory/src/test/java/com/fjb/inventory/InventoryApplicationTests.java
+++ inventory/src/test/java/com/fjb/inventory/InventoryApplicationTests.java
@@ -1,13 +1,16 @@
 package com.fjb.inventory;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class InventoryApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("Just test");
 	}
 
 }
