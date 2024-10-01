ac494f427ce5542193f1609b423edbb0c5fcf51f
Khanh Du <110228244+khanhduzz@users.noreply.github.com>
2024-10-01 13:25:56 +0700

    fix sonar (#31)

--- order/src/test/java/com/fjb/order/OrderApplicationTests.java
+++ order/src/test/java/com/fjb/order/OrderApplicationTests.java
@@ -1,13 +1,16 @@
 package com.fjb.order;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class OrderApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("Just test");
     }
 
 }
--- product/src/test/java/com/fjb/product/ProductApplicationTests.java
+++ product/src/test/java/com/fjb/product/ProductApplicationTests.java
@@ -1,13 +1,16 @@
 package com.fjb.product;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class ProductApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("Just test");
 	}
 
 }
