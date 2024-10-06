e710e917fa08ca23aa4d773402bbdb3d66cc345d
Khanh Du <luongkhanhdu95@gmail.com>
2024-10-06 08:53:17 +0700

    add bff for redirect service in sell:

---
+++ sell-bff/src/test/java/com/fjb/sellbff/SellBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fjb.sellbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellBffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
