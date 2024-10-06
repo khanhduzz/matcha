19140d87d5f7622a932109a8416639036ee2cbb8
Khanh Du <110228244+khanhduzz@users.noreply.github.com>
2024-10-06 08:54:59 +0700

    add bff for redirect service in sell: (#46)

---
+++ sell-bff/src/main/java/com/fjb/sellbff/SellBffApplication.java
@@ -0,0 +1,13 @@
+package com.fjb.sellbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellBffApplication.class, args);
+    }
+
+}
