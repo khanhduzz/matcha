06a77501a84d865f363cba35038a09e093462187
Khanh Du <luongkhanhdu95@gmail.com>
2024-10-11 15:28:28 +0700

    config sonar with cors

--- product/src/main/java/com/fjb/product/config/CorsConfig.java
+++ product/src/main/java/com/fjb/product/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.fjb.product.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${cors.allowed}")
+    private String corsAllowed;
+
     @Bean
     public WebMvcConfigurer corsConfigure() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*")
-                    .allowedOrigins("*").allowedHeaders("*");
+                registry.addMapping("/**").allowedMethods(corsAllowed)
+                    .allowedOrigins(corsAllowed).allowedHeaders(corsAllowed);
             }
         };
     }
