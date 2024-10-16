0537140245e048d2c0c62ac8aae5f19ed56d91b6
Khanh Du <luongkhanhdu95@gmail.com>
2024-10-16 15:36:40 +0700

    add temporal welcome page after login

---
+++ sell-bff/src/main/java/com/fjb/sellbff/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.fjb.sellbff.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class HealthController {
+    @GetMapping
+    public String healthController() {
+        return "index";
+    }
+}
